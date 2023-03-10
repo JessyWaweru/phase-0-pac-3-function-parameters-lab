607dc804cf303f2303660279f42b5d6707b4f49b
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-03-11 01:41:06 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+// name='Aki'
+function introduction(name){
+    //let name ='Aki.'
+   // const name='Aki'
+    // console.log('Hi, my name is ${name}')
+     
+     //const name='Aki'
+   return `Hi, my name is ${name}.`
+   
+ //  const name='Aki.'
+
+
+
+}
+  
+function introductionWithLanguage(name,language){
+ return  `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name,language='JavaScript'){
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
